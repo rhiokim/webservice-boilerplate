0b96eeabe4d9ea0820e1a37569febc7b8ced61e4
rhiokim <rhio.kim@gmail.com>
2012-02-09 16:05:00 +0900

    html, js 초기화

--- www/js/main.js
+++ www/js/main.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-    $('button._show_sc_select_form').click(function(e) {
-		$('._sc_select_form').css({marginTop:-700}).show().stop().animate({
-			marginTop: 0
-		}, 500);
-    });
-
-    $('button._close_sc_select_form').click(function(e) {
-        $('._sc_select_form').slideUp();
-    });
 
 });
 
